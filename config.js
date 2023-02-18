2c9daf46110cf0cd6b069342d5b4d065820f59c2
MarsClaim <Luivil777@gmail.com>
2023-02-17 18:55:15 -0600

    second commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.export ={
+    "databases" : [
+    {"name" : 'development',
+    "server" : 'localhost',
+    "port" : 1433,
+    "databases" : 'test',
+    "options" : {"trustServerCertificate" : true},
+    "user": 'remote',
+    "password" : 'jalap4'
+
+    }]
+}
--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const bodyparser = require ("body-parser");
 const app = express(); //it is required type the const app which is the one used to creste a whole express app which is the representation of express
 const cors = require("cors");
 const env =require("dotenv");
+const conn =  require("./config");
+const sql = require("mssql");
+const bee = require("bcrypt");
+
+
 
 env.config({
     path:"./env.env",
@@ -22,9 +27,111 @@ app.all("*", (req, res, next) => {
 //body tends to go the info which will be used to control and header to show to user
 
 
+const conn2 = {"name": 'development',
+"server":'localhost',
+"port": 1433,
+"database" : 'test',
+"user": 'remote',
+"password" : 'jalap4'
+}
+
+async function sql_run(req, res, command){
+    //req.body
+    //let cred = req.body;
+    const pool = new sql.ConnectionPool(conn2);
+    //const pool = new sql.ConnectionPool(conn.databases);
+    pool.on("error", err => {console.log(err)});
+    //pool.on("success", succ => {console.log(succ)}
+    try{
+        await pool.connect()
+        let result = await pool.request().query(command);
+
+        
+        return {
+            "success": result
+        };
+    }
+    catch (err){
+        console.log(err);
+        return err;
+    }
+    finally{
+        pool.close();
+    }
+};
+
 app.get('/', function(req, res){ //Its an endpoint that will respond to the request done in it, according to the '/' and the place put there
-    res.send("HelooWorld");
+    res.send("HelloWorld");
 });
+
+app.post('/insert', function(req, res){//EndPoint to insert data
+    let cred = req.body;
+    let command = `insert into users(username, pass, email) values ('${cred.user}', '${hash}', '${cred.email}')`;
+    sql_run(req, res, command);
+    //res.send(sql_run());
+
+    //res.send("DataInserted");
+});
+
+app.post('/read', function(req, res){//EndPoint to delete data
+    let cred = req.body;
+    let command = `select * from users`;
+    sql_run(req, res, command);
+});
+
+app.post('/delete', function(req, res){//EndPoint to delete data
+    let cred = req.body;
+    let command = `delete from users where id = ${cred.id}`;
+    sql_run(req, res, command);
+    //res.send(sql_run());
+
+    //res.send("DataInserted");
+});
+
+app.post('/update', function(req, res){//EndPoint to delete data
+    let cred = req.body;
+    let command = `update users set ${cred.column} = ${cred.finder} where ${cred.ident} = ${cred.ident}`;
+    sql_run(req, res, command);
+    //res.send(sql_run());
+
+    //res.send("DataInserted");
+});
+
+app.post('/register', function(req, res){
+    let cred = req.body;
+    bee.hash(cred.pass, 10, async function(err, hash){
+        let command = `insert into users(username, pass, email) values ('${cred.user}', '${hash}', '${cred.email}')`;
+        sql_run(req, res, command);
+    });//Everything has to pass through this to encrypt it(#10 the way and times it will be mixing up)
+});
+
+app.post('/login', async function(req, res){
+    let cred = req.body;
+    let command = `findUser '${cred.user}'`;
+    let result = await sql_run (req, res, command);
+    let comp = await bee.compare(cred.pass, result.success.recordset[0].pass, function (err, _res) {
+        if (_res == true){
+            if (result.recordset.lenght < 0){
+                res.send('USER NOT FOUND');
+            }else{
+                let _u = result.recordset[0];
+            }
+        }
+    })
+    console.log=(result);
+    res.send(result);
+    //res.send(comp);
+});
+
+
+const port = process.env.PORT||3000;
+app.listen(port,()=> {
+    //console.log("App is runnning on port:" + Port)
+    console.log(`App is runnning on port: ${port}`);
+});//It is for the requests to listen to this program
+
+
+
 
 app.get('/home', function(req, res){ 
     res.send("Mikasa");
@@ -94,11 +201,6 @@ app.put("/updateuser", function(req, res){
 app.get("/getallusers", function(req, res){
     res.send(db);
 });
-const port = process.env.PORT||3000;
-app.listen(port,()=> {
-    //console.log("App is runnning on port:" + Port)
-    console.log(`App is runnning on port: ${port}`);
-});//It is for the requests to listen to this program
 
 
 
